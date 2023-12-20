886d5a7f24105d71e4937b47871bd3ed70b1ab38
Pra3t0r5 <fernando.albertengo@gmail.com>
2023-12-19 22:30:43 -0300

    Muestra de abstraccion de nuevos componentes

---
+++ app/constants.ts
@@ -0,0 +1,21 @@
+export const LINKS = [
+  {
+    title: "Docs",
+    description: "Find in-depth information about Next.js features and API.",
+    link: "https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app",
+  },
+  {
+    title: "Learn",
+    description:
+      "FLearn about Next.js in an interactive course with&nbsp;quizzes!",
+  },
+  {
+    title: "Templates",
+    description: "Explore starter templates for Next.js.",
+  },
+  {
+    title: "Deploy",
+    description:
+      "Instantly deploy your Next.js site to a shareable URL with Vercel.",
+  },
+];
